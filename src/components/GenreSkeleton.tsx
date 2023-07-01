d4445f5b27030c968f8cafc8c4dd35c13575ea83
Piotr Z <pzdanowiczp@gamil.com>
2023-07-01 20:40:57 +0200

    Show a skeleton while fetching the genres

---
+++ src/components/GenreSkeleton.tsx
@@ -0,0 +1,18 @@
+import { HStack, List, Skeleton, SkeletonText } from '@chakra-ui/react';
+
+const GenreSkeleton = () => {
+  const skeletons = [1, 2, 3, 4, 5, 6];
+
+  return (
+    <List>
+      {skeletons.map((skeleton) => (
+        <HStack>
+          <Skeleton boxSize='32px' borderRadius={8} />
+          <SkeletonText fontSize='lg'></SkeletonText>
+        </HStack>
+      ))}
+    </List>
+  );
+};
+
+export default GenreSkeleton;
